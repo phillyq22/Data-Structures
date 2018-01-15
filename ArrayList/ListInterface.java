4baabd6e071a6e53f34bf12763ea93b1500ff5af
phillyq22 <quinnp6@students.rowan.edu>
2018-01-15 11:54:23 -0500

    Updated ReadMe

--- ArrayList/ListInterface.java
+++ ArrayList/ListInterface.java
@@ -12,11 +12,8 @@ public interface ListInterface<T>
 {
     boolean isEmpty();
     int size();
-    void add(int index, T item)
-    throws ListIndexOutOfBoundsException;
-    Object get(int index)
-    throws ListIndexOutOfBoundsException;
-    void remove(int index)
-    throws ListIndexOutOfBoundsException;
+    void add(int index, T item) throws ListIndexOutOfBoundsException;
+    T get(int index) throws ListIndexOutOfBoundsException;
+    void remove(int index)throws ListIndexOutOfBoundsException;
     void removeAll();
 }  // end ListInterface
